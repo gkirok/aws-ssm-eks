6e19f1c8ebe43d2c241675bd4c6adc8096c1b3ce
Gall Zvyagin <gkirok@gmail.com>
2022-09-04 23:24:51 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-debug() {
-  if [ "${ACTIONS_RUNNER_DEBUG:-}" = "true" ]; then
-    echo "DEBUG: :: $*" >&2
-  fi
-}
-
 if [ -n "${INPUT_AWS_ACCESS_KEY_ID:-}" ]; then
   export AWS_ACCESS_KEY_ID="${INPUT_AWS_ACCESS_KEY_ID}"
 fi
@@ -30,10 +24,14 @@ else
   aws eks update-kubeconfig --name "${INPUT_CLUSTER_NAME}"
 fi
 
-debug "Cluster: $INPUT_CLUSTER_NAME";
+echo "Cluster: $INPUT_CLUSTER_NAME";
+echo "Bastion name: $INPUT_BASTION_NAME";
+echo "Bastion id: $INPUT_BASTION_ID";
+echo "Bastion name: $BASTION_NAME";
+echo "Bastion id: $BASTION_ID";
 
 if [ -n "${INPUT_BASTION_NAME}" ]; then
-  debug "Bastion: $INPUT_BASTION_NAME";
+  echo "Bastion: $INPUT_BASTION_NAME";
   export INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${INPUT_BASTION_NAME}" "Name=instance-state-code,Values=16" --output text --query 'Reservations[*].Instances[*].InstanceId')
 elif [ -n "${INPUT_BASTION_ID}" ]; then
   export INSTANCE_ID=$INPUT_BASTION_ID
@@ -41,30 +39,30 @@ else
   echo "Required: BASTION_NAME or BASTION_ID"
   exit 1
 fi
-debug "InstanceId: $INSTANCE_ID";
+echo "InstanceId: $INSTANCE_ID";
 
 CLUSTER_API=$(aws eks describe-cluster --name $INPUT_CLUSTER_NAME | jq -r '.cluster.endpoint' | awk -F/ '{print $3}')
-debug "Cluster API: $CLUSTER_API";
+echo "Cluster API: $CLUSTER_API";
 
 if [ -n "${INPUT_SSM_PORT:-}" ]; then
   PORT=$INPUT_SSM_PORT
 else
   PORT="$((8 + $RANDOM % 25))443"
 fi
-debug "Port: $PORT"
+echo "Port: $PORT"
 
-debug "Update /etc/hosts"
+echo "Update /etc/hosts"
 sudo sh -c "echo '127.0.0.1 ${CLUSTER_API}' >> /etc/hosts"
 
-debug "Update ~/.kube/config"
+echo "Update ~/.kube/config"
 sed -i -e "s/https:\/\/$CLUSTER_API/https:\/\/$CLUSTER_API:$PORT/" ~/.kube/config
 
 CLUSTER_ARN=$(aws eks describe-cluster --name $INPUT_CLUSTER_NAME | jq -r '.cluster.arn')
-debug "Cluster ARN: $CLUSTER_ARN"
+echo "Cluster ARN: $CLUSTER_ARN"
 kubectl config use-context $CLUSTER_ARN
 
-debug "Starting session"
+echo "Starting session"
 output=$( nohup aws ssm start-session --target ${INSTANCE_ID} --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters "{\"host\": [ \"${CLUSTER_API}\" ], \"portNumber\": [ \"443\" ], \"localPortNumber\": [ \"$PORT\" ] }" & )
-debug "${output}"
+echo "${output}"
 sleep 10
 echo ::set-output name=ssm-out::"${output}"
