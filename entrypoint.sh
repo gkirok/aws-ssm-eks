8e13cf2b2d8fa572bfab902eb13bcf34c9427d90
Gall Zvyagin <gkirok@gmail.com>
2022-09-04 23:28:38 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,51 +1,39 @@
 #!/bin/sh
 
-if [ -n "${INPUT_AWS_ACCESS_KEY_ID:-}" ]; then
-  export AWS_ACCESS_KEY_ID="${INPUT_AWS_ACCESS_KEY_ID}"
+if [ -n "${AWS_ACCESS_KEY_ID:-}" ]; then
+  export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
 fi
 
-if [ -n "${INPUT_AWS_SECRET_ACCESS_KEY:-}" ]; then
-  export AWS_SECRET_ACCESS_KEY="${INPUT_AWS_SECRET_ACCESS_KEY}"
+if [ -n "${AWS_SECRET_ACCESS_KEY:-}" ]; then
+  export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
 fi
 
-if [ -n "${INPUT_AWS_REGION:-}" ]; then
-  export AWS_DEFAULT_REGION="${INPUT_AWS_REGION}"
+if [ -n "${AWS_REGION:-}" ]; then
+  export AWS_DEFAULT_REGION="${AWS_REGION}"
 fi
 
 echo "aws version"
-
 aws --version
 
 echo "Attempting to update kubeconfig for aws"
+aws eks update-kubeconfig --name "${CLUSTER_NAME}"
 
-if [ -n "${INPUT_EKS_ROLE_ARN}" ]; then
-  aws eks update-kubeconfig --name "${INPUT_CLUSTER_NAME}" --role-arn "${INPUT_EKS_ROLE_ARN}"
-else
-  aws eks update-kubeconfig --name "${INPUT_CLUSTER_NAME}"
-fi
-
-echo "Cluster: $INPUT_CLUSTER_NAME";
-echo "Bastion name: $INPUT_BASTION_NAME";
-echo "Bastion id: $INPUT_BASTION_ID";
-echo "Bastion name: $BASTION_NAME";
-echo "Bastion id: $BASTION_ID";
-
-if [ -n "${INPUT_BASTION_NAME}" ]; then
-  echo "Bastion: $INPUT_BASTION_NAME";
-  export INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${INPUT_BASTION_NAME}" "Name=instance-state-code,Values=16" --output text --query 'Reservations[*].Instances[*].InstanceId')
-elif [ -n "${INPUT_BASTION_ID}" ]; then
-  export INSTANCE_ID=$INPUT_BASTION_ID
+if [ -n "${BASTION_NAME}" ]; then
+  echo "Bastion: $BASTION_NAME";
+  export INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${BASTION_NAME}" "Name=instance-state-code,Values=16" --output text --query 'Reservations[*].Instances[*].InstanceId')
+elif [ -n "${BASTION_ID}" ]; then
+  export INSTANCE_ID=$BASTION_ID
 else
   echo "Required: BASTION_NAME or BASTION_ID"
   exit 1
 fi
 echo "InstanceId: $INSTANCE_ID";
 
-CLUSTER_API=$(aws eks describe-cluster --name $INPUT_CLUSTER_NAME | jq -r '.cluster.endpoint' | awk -F/ '{print $3}')
+CLUSTER_API=$(aws eks describe-cluster --name $CLUSTER_NAME | jq -r '.cluster.endpoint' | awk -F/ '{print $3}')
 echo "Cluster API: $CLUSTER_API";
 
-if [ -n "${INPUT_SSM_PORT:-}" ]; then
-  PORT=$INPUT_SSM_PORT
+if [ -n "${SSM_PORT:-}" ]; then
+  PORT=$SSM_PORT
 else
   PORT="$((8 + $RANDOM % 25))443"
 fi
@@ -57,7 +45,7 @@ sudo sh -c "echo '127.0.0.1 ${CLUSTER_API}' >> /etc/hosts"
 echo "Update ~/.kube/config"
 sed -i -e "s/https:\/\/$CLUSTER_API/https:\/\/$CLUSTER_API:$PORT/" ~/.kube/config
 
-CLUSTER_ARN=$(aws eks describe-cluster --name $INPUT_CLUSTER_NAME | jq -r '.cluster.arn')
+CLUSTER_ARN=$(aws eks describe-cluster --name $CLUSTER_NAME | jq -r '.cluster.arn')
 echo "Cluster ARN: $CLUSTER_ARN"
 kubectl config use-context $CLUSTER_ARN
 
